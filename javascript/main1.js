cc798077adb84f33b532c7d0fcd03d192231dc28
5WCP <oldman22660@gmail.com>
2023-04-24 14:54:30 +0800

    隨機餐廳

---
+++ javascript/main1.js
@@ -0,0 +1,679 @@
+// 物件
+
+// const divorce = {
+//     "success": true,
+//     "result": {
+//     "resource_id": "301000000A-002072-001",
+//     "limit": 2000,
+//     "total": 45,
+//     "fields": [
+//     {
+//     "type": "text",
+//     "id": "statistic_yyy"
+//     },
+//     {
+//     "type": "text",
+//     "id": "according"
+//     },
+//     {
+//     "type": "text",
+//     "id": "district_code"
+//     },
+//     {
+//     "type": "text",
+//     "id": "site_id"
+//     },
+//     {
+//     "type": "text",
+//     "id": "sex"
+//     },
+//     {
+//     "type": "text",
+//     "id": "average_age"
+//     }
+//     ],
+//     "records": [
+//     {
+//     "statistic_yyy": "統計年",
+//     "according": "按照別",
+//     "district_code": "區域別代碼",
+//     "site_id": "區域別",
+//     "sex": "性別",
+//     "average_age": "平均年齡"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "65000000",
+//     "site_id": "新北市",
+//     "sex": "男",
+//     "average_age": "33.0"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "65000000",
+//     "site_id": "新北市",
+//     "sex": "女",
+//     "average_age": "31.1"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "63000000",
+//     "site_id": "臺北市",
+//     "sex": "男",
+//     "average_age": "33.9"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "63000000",
+//     "site_id": "臺北市",
+//     "sex": "女",
+//     "average_age": "32.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "68000000",
+//     "site_id": "桃園市",
+//     "sex": "男",
+//     "average_age": "31.9"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "68000000",
+//     "site_id": "桃園市",
+//     "sex": "女",
+//     "average_age": "30.0"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "66000000",
+//     "site_id": "臺中市",
+//     "sex": "男",
+//     "average_age": "32.0"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "66000000",
+//     "site_id": "臺中市",
+//     "sex": "女",
+//     "average_age": "30.2"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "67000000",
+//     "site_id": "臺南市",
+//     "sex": "男",
+//     "average_age": "32.5"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "67000000",
+//     "site_id": "臺南市",
+//     "sex": "女",
+//     "average_age": "30.5"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "64000000",
+//     "site_id": "高雄市",
+//     "sex": "男",
+//     "average_age": "32.5"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "64000000",
+//     "site_id": "高雄市",
+//     "sex": "女",
+//     "average_age": "30.5"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10002000",
+//     "site_id": "宜蘭縣",
+//     "sex": "男",
+//     "average_age": "31.4"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10002000",
+//     "site_id": "宜蘭縣",
+//     "sex": "女",
+//     "average_age": "29.5"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10004000",
+//     "site_id": "新竹縣",
+//     "sex": "男",
+//     "average_age": "31.8"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10004000",
+//     "site_id": "新竹縣",
+//     "sex": "女",
+//     "average_age": "30.0"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10005000",
+//     "site_id": "苗栗縣",
+//     "sex": "男",
+//     "average_age": "31.6"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10005000",
+//     "site_id": "苗栗縣",
+//     "sex": "女",
+//     "average_age": "29.8"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10007000",
+//     "site_id": "彰化縣",
+//     "sex": "男",
+//     "average_age": "31.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10007000",
+//     "site_id": "彰化縣",
+//     "sex": "女",
+//     "average_age": "29.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10008000",
+//     "site_id": "南投縣",
+//     "sex": "男",
+//     "average_age": "31.0"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10008000",
+//     "site_id": "南投縣",
+//     "sex": "女",
+//     "average_age": "29.2"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10009000",
+//     "site_id": "雲林縣",
+//     "sex": "男",
+//     "average_age": "31.1"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10009000",
+//     "site_id": "雲林縣",
+//     "sex": "女",
+//     "average_age": "29.1"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10010000",
+//     "site_id": "嘉義縣",
+//     "sex": "男",
+//     "average_age": "31.2"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10010000",
+//     "site_id": "嘉義縣",
+//     "sex": "女",
+//     "average_age": "29.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10013000",
+//     "site_id": "屏東縣",
+//     "sex": "男",
+//     "average_age": "31.7"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10013000",
+//     "site_id": "屏東縣",
+//     "sex": "女",
+//     "average_age": "29.4"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10014000",
+//     "site_id": "臺東縣",
+//     "sex": "男",
+//     "average_age": "31.2"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10014000",
+//     "site_id": "臺東縣",
+//     "sex": "女",
+//     "average_age": "29.5"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10015000",
+//     "site_id": "花蓮縣",
+//     "sex": "男",
+//     "average_age": "31.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10015000",
+//     "site_id": "花蓮縣",
+//     "sex": "女",
+//     "average_age": "29.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10016000",
+//     "site_id": "澎湖縣",
+//     "sex": "男",
+//     "average_age": "31.2"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10016000",
+//     "site_id": "澎湖縣",
+//     "sex": "女",
+//     "average_age": "29.3"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10017000",
+//     "site_id": "基隆市",
+//     "sex": "男",
+//     "average_age": "32.4"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10017000",
+//     "site_id": "基隆市",
+//     "sex": "女",
+//     "average_age": "30.6"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10018000",
+//     "site_id": "新竹市",
+//     "sex": "男",
+//     "average_age": "32.6"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10018000",
+//     "site_id": "新竹市",
+//     "sex": "女",
+//     "average_age": "30.7"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10020000",
+//     "site_id": "嘉義市",
+//     "sex": "男",
+//     "average_age": "32.4"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "10020000",
+//     "site_id": "嘉義市",
+//     "sex": "女",
+//     "average_age": "30.1"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "09020000",
+//     "site_id": "金門縣",
+//     "sex": "男",
+//     "average_age": "31.7"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "09020000",
+//     "site_id": "金門縣",
+//     "sex": "女",
+//     "average_age": "30.1"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "09007000",
+//     "site_id": "連江縣",
+//     "sex": "男",
+//     "average_age": "32.0"
+//     },
+//     {
+//     "statistic_yyy": "110",
+//     "according": "按發生日期分",
+//     "district_code": "09007000",
+//     "site_id": "連江縣",
+//     "sex": "女",
+//     "average_age": "30.0"
+//     }
+//     ]
+//     }
+//     }
+
+// console.log(divorce.result.records[1].average_age);
+
+// divorce.result.records.forEach((i) => {
+//     console.log(i.average_age);
+// });
+
+// const person = {
+//     name: "john" ,
+//     age: 23,
+//     email:[
+//         "xd@gmail.com",
+//         "xdd@gmal.com",
+//     ],
+//     address:"小民",
+//     pet:[
+//         {
+//             name: "mary",
+//             age: 6
+//         },
+//         {
+//             name: "katty",
+//             age: 9
+//         },
+//     ]
+// }
+
+// console.log(person.email);
+// person.pet.forEach(function(i){
+//     console.log(i.name);
+// });
+
+// 彈性的
+
+// const obj = {
+//     name: "john",
+//     fn: function() {
+//         console.log("這是 obj 裡面的方法");
+//     }
+// }
+
+// console.log(obj.fn());
+
+// const menuList = [
+//     {
+//         "name": "食在一方",
+//         "category": ["brunch"],
+//         "filter": false
+//     },
+//     {
+//         "name": "日十",
+//         "category": ["brunch"],
+//         "filter": false
+//     },
+//     {
+//         "name": "鹽行站",
+//         "category": ["buffet", "rice"],
+//         "filter": false
+//     },
+//     {
+//         "name": "鍋燒麵",
+//         "category": ["noodle"],
+//         "filter": false
+//     },
+//     {
+//         "name": "炒飯",
+//         "category": ["rice"],
+//         "filter": false
+//     },
+//     {
+//         "name": "八方雲集",
+//         "category": ["others"],
+//         "filter": false
+//     },
+//     {
+//         "name": "火鍋",
+//         "category": ["others"],
+//         "filter": false
+//     },
+//     {
+//         "name": "韓式料理",
+//         "category": ["rice", "noodle"],
+//         "filter": false
+//     },
+//     {
+//         "name": "小飯糰大飯糰",
+//         "category": ["rice"],
+//         "filter": false
+//     },
+//     {
+//         "name": "後校門滷肉飯",
+//         "category": ["rice", "noodle"],
+//         "filter": false
+//     },
+//     {
+//         "name": "魚耶",
+//         "category": ["brunch", "rice", "noodle"],
+//         "filter": false
+//     },
+//     {
+//         "name": "吳家鴨香飯",
+//         "category": ["rice", "noodle"],
+//         "filter": false
+//     }
+// ]
+
+// const random = Math.floor(Math.random() * menuList.length);
+
+// console.log(random);
+
+// console.log(menuList[random].name);
+
+// menuList.forEach((i) =>{
+//     console.log(i.name);
+// });
+
+const menuList = [
+    {
+        "name": "食在一方",
+        "category": ["brunch"],
+        "filter": false
+    },
+    {
+        "name": "日十",
+        "category": ["brunch"],
+        "filter": false
+    },
+    {
+        "name": "鹽行站",
+        "category": ["buffet", "rice"],
+        "filter": false
+    },
+    {
+        "name": "鍋燒麵",
+        "category": ["noodle"],
+        "filter": false
+    },
+    {
+        "name": "炒飯",
+        "category": ["rice"],
+        "filter": false
+    },
+    {
+        "name": "八方雲集",
+        "category": ["others"],
+        "filter": false
+    },
+    {
+        "name": "火鍋",
+        "category": ["others"],
+        "filter": false
+    },
+    {
+        "name": "韓式料理",
+        "category": ["rice", "noodle"],
+        "filter": false
+    },
+    {
+        "name": "小飯糰大飯糰",
+        "category": ["rice"],
+        "filter": false
+    },
+    {
+        "name": "後校門滷肉飯",
+        "category": ["rice", "noodle"],
+        "filter": false
+    },
+    {
+        "name": "魚耶",
+        "category": ["brunch", "rice", "noodle"],
+        "filter": false
+    },
+    {
+        "name": "吳家鴨香飯",
+        "category": ["rice", "noodle"],
+        "filter": false
+    },
+    {
+        "name": "神武拉麵",
+        "category": ["noodle"],
+        "filter": false
+    },
+    {
+        "name": "王牌便當",
+        "category": ["rice"],
+        "filter": false
+    },
+    {
+        "name": "一品屋",
+        "category": ["rice"],
+        "filter": false
+    },
+    {
+        "name": "喜得",
+        "category": ["brunch"],
+        "filter": false
+    }
+]
+
+const brun = document.querySelector("#brunch");
+const ric = document.querySelector("#rice");
+const nood = document.querySelector("#noodle");
+const buff = document.querySelector("#buffet");
+const oth = document.querySelector("#other");
+const button = document.querySelector("#btn");
+const resp = document.querySelector("#response");
+
+button.addEventListener("click" , function(i){
+    let chArr = [];
+    menuList.forEach((i)=>{
+        i.filter = false;
+    });
+    if(brun.checked) {
+        menuList.forEach((x)=>{
+            x.category.forEach((y)=>{
+                if(y==="brunch"){
+                    x.filter = true;
+                }
+            });
+        });
+    }
+
+    if(ric.checked) {
+        menuList.forEach((x)=>{
+            x.category.forEach((y)=>{
+                if(y==="rice"){
+                    x.filter = true;
+                }
+            });
+        });
+    }
+
+    if(nood.checked) {
+        menuList.forEach((x)=>{
+            x.category.forEach((y)=>{
+                if(y==="noodle"){
+                    x.filter = true;
+                }
+            });
+        });
+    }
+
+    if(buff.checked) {
+        menuList.forEach((x)=>{
+            x.category.forEach((y)=>{
+                if(y==="buffet"){
+                    x.filter = true;
+                }
+            });
+        });
+    }
+
+    if(oth.checked) {
+        menuList.forEach((x)=>{
+            x.category.forEach((y)=>{
+                if(y==="others"){
+                    x.filter = true;
+                }
+            });
+        });
+    }
+
+    menuList.forEach((i)=>{
+        if(i.filter == true){
+            chArr.push(i.name);
+        }
+    });
+
+    if(chArr.length !== 0){
+        const random = Math.floor(Math.random() * chArr.length);
+        resp.innerText = "吃爆 " + chArr[random] + "!!!!!!!!";
+    } else {
+        resp.innerText = "做仙!!!!!!";
+    }
+});
+
+// setTimeOut
+
+setTimeout(function() {
+    resp.innerText = "這麼猶豫!?";
+},5000)
